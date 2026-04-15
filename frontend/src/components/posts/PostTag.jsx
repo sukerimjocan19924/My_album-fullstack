dc8545149c0262a00a394be5608c7b3a5dff9f37
HSR <hseoglan@naver.com>
2026-04-15 21:28:39 +0900

    8차 업데이트(로그인-router_보호영역부분 수정)

---
+++ frontend/src/components/posts/PostTag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostTag = () => {
+  return (
+    <div>PostTag</div>
+  )
+}
+
+export default PostTag
---
+++ frontend/src/pages/posts/PostDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PostHeader from '@/components/posts/PostHeader'
+
+const PostDashboard = () => {
+  return (
+    <section className='page post-section'>
+      <div className="inner">
+        <PostHeader />
+      </div>
+    </section>
+  )
+}
+
+export default PostDashboard
