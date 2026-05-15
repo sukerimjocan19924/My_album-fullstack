1e5297007f9f0f64b59e8155c3804bc00831cfa9
HSR <hseoglan@naver.com>
2026-05-15 18:37:44 +0900

    9차 업데이트(frontend-새 앨범 추가)

--- frontend/src/components/posts/PostHeader.jsx
+++ frontend/src/components/posts/PostHeader.jsx
@@ -5,7 +5,7 @@ const PostHeader = ({onCreate}) => {
   return (
     <header className='post-header'>
       <h2 className='post-title'>새 앨범을 작성하세요</h2>
-      <Button text="새 앨범 작성" className="primary" icons/>
+      <Button text="새 앨범 작성" className="primary" onClick={onCreate} icons/>
     </header>
   )
 }
